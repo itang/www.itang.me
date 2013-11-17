a021e9608ccaffe7ebb7f69d53d1b7e29de7abbb
itang <live.tang@gmail.com>
2013-11-17 10:10:12 +0800

    cleanup

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+rm -rf github.com/bmizerany
+rm -rf github.com/google
+rm -rf code.google.com
+
 git clone https://github.com/bmizerany/pat.git github.com/bmizerany/pat
+rm -rf github.com/bmizerany/pat/.git
+rm -rf github.com/bmizerany/pat/example
 
 git clone git@github.com:google/go-querystring.git github.com/google/go-querystring
-git clone git@github.com:google/go-github.git github.com/google/go-github
+rm -rf github.com/google/go-querystring/.git
 
+git clone git@github.com:google/go-github.git github.com/google/go-github
+rm -rf github.com/google/go-github/.git
+
+hg clone https://code.google.com/p/goauth2/ code.google.com/p/goauth2
+rm -rf code.google.com/p/goauth2/appengine
+rm -rf code.google.com/p/goauth2/lib
+rm -rf code.google.com/p/goauth2/oauth/jwt
+rm -rf code.google.com/p/goauth2/oauth/example
